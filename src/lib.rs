dea20f70abcf667854e9c01fd63f85f7f17f2894
uriegel <uriegel@hotmail.de>
2021-07-13 22:41:46 +0200

    Readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # systemicons
 //! 
-//! With this lib you can retrive the system icon which is associated 
+//! With this lib you can retrieve the system icon which is associated 
 //! to a certain file extension. The icon will be in the .png format. 
 //! Windows and Linux (GTK) are supported.
 //! 
